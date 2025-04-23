29657b8fa092e71cf1df9d45fe96cb05878b5e09
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-04-23 10:57:43 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,26 +3,31 @@ import 'dotenv/config';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
 
+// Pobieramy zmienne środowiskowe
 const { APP_ID, GUILD_ID, BOT_TOKEN } = process.env;
 
-if (!BOT_TOKEN || !CLIENT_ID || !GUILD_ID) {
-  console.error('❌ Missing one of required env vars: BOT_TOKEN, CLIENT_ID, GUILD_ID');
+// Weryfikacja obecności wymaganych zmiennych
+if (!BOT_TOKEN || !APP_ID || !GUILD_ID) {
+  console.error('❌ Missing one of required env vars: BOT_TOKEN, APP_ID, GUILD_ID');
   process.exit(1);
 }
 
+// Definicja komend
 const commands = [
   { name: 'stats', description: 'Pobierz statystyki wszystkich Twoich kont' },
   { name: 'token', description: 'Wygeneruj swój API Token' }
 ];
 
+// Inicjalizacja REST clienta
 const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
 
 (async () => {
   try {
     console.log('🔄 Refreshing slash commands...');
 
+    // Rejestracja komend na poziomie serwera testowego (guild)
     await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      Routes.applicationGuildCommands(APP_ID, GUILD_ID),
       { body: commands }
     );
 
